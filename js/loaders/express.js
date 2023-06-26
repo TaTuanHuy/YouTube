f9d3d0bc23af3aab3a79787c90732f77d42ea382
TaTuanHuy <89507792+TaTuanHuy@users.noreply.github.com>
2023-06-26 03:31:49 -0700

    redis

--- js/loaders/express.js
+++ js/loaders/express.js
@@ -10,12 +10,60 @@ const port = config_1.default.port;
 const mainRouter_1 = __importDefault(require("../api/router/mainRouter"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
-exports.default = ({ app }) => {
+// import fetch from "node-fetch";
+// import redis from "redis";
+const axios_1 = __importDefault(require("axios"));
+const redis_1 = require("redis");
+exports.default = async ({ app }) => {
     app.use(express_1.default.json());
     app.use(express_1.default.urlencoded({ extended: true }));
     app.use((0, cookie_parser_1.default)());
     app.use((0, cors_1.default)());
     app.use(express_1.default.static("../src/public"));
+    let redisClient;
+    (async () => {
+        redisClient = (0, redis_1.createClient)({
+            // url: "redis://alice:foobared@awesome.redis.server:6380",
+            socket: {
+                host: "127.0.0.1",
+                port: 6379,
+            },
+        });
+        redisClient.on("error", (error) => console.error(`Error : ${error}`));
+        redisClient.on("connect", () => {
+            console.log("Connected!");
+        });
+        await redisClient.connect();
+    })();
+    //Getdata to set cache
+    async function getData() {
+        const result = await axios_1.default.get(`http://localhost:8000`);
+        console.log(result.data);
+        return result.data;
+    }
+    async function getSpeciesData(req, res) {
+        let isCached = false;
+        let results;
+        try {
+            const cacheResults = await redisClient.get("test");
+            if (cacheResults) {
+                isCached = true;
+                results = JSON.parse(cacheResults);
+            }
+            else {
+                results = await getData();
+                if (results.length === 0) {
+                    throw "API returned an empty array";
+                }
+                await redisClient.set("test", JSON.stringify(results));
+            }
+            return res.json(results.length);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    app.get("/testRedis", getSpeciesData);
     (0, mainRouter_1.default)(app);
     app.listen(port, () => {
         console.log(`Example app listening on port http://localhost:${port}`);
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -5,17 +5,71 @@ const port = config.port;
 import router from "../api/router/mainRouter";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+// import fetch from "node-fetch";
+// import redis from "redis";
+import axios from "axios";
+import { Request, Response, NextFunction } from "express";
+import { RedisClientType } from "@redis/client";
+import { createClient } from "redis";
 
-export default ({ app }: { app: express.Application }) => {
+export default async ({ app }: { app: express.Application }) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
   app.use(cors());
-
   app.use(express.static("../src/public"));
 
-  router(app);
+  let redisClient: RedisClientType;
 
+  (async () => {
+    redisClient = createClient({
+      // url: "redis://alice:foobared@awesome.redis.server:6380",
+      socket: {
+        host: "127.0.0.1",
+        port: 6379,
+      },
+    });
+
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+    redisClient.on("connect", () => {
+      console.log("Connected!");
+    });
+
+    await redisClient.connect();
+  })();
+
+  //Getdata to set cache
+  async function getData() {
+    const result = await axios.get(`http://localhost:8000`);
+    console.log(result.data);
+    return result.data;
+  }
+
+  async function getSpeciesData(req: Request, res: Response) {
+    let isCached = false;
+    let results;
+    try {
+      const cacheResults = await redisClient.get("test");
+      if (cacheResults) {
+        isCached = true;
+        results = JSON.parse(cacheResults);
+      } else {
+        results = await getData();
+        if (results.length === 0) {
+          throw "API returned an empty array";
+        }
+        await redisClient.set("test", JSON.stringify(results));
+      }
+      return res.json(results.length);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  app.get("/testRedis", getSpeciesData);
+
+  router(app);
   app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
   });
